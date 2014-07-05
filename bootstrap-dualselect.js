28c84a2c92ba5f9ccce3907914173e683db208be
Tomasz <tomasz@stechly.com>
2014-07-05 06:59:38 -0700

    Use ngModel and ngOptions to get selectable and selected array reference.

--- bootstrap-dualselect.js
+++ bootstrap-dualselect.js
@@ -1,17 +1,20 @@
 'use strict';
 
+var scripts = document.getElementsByTagName("script")
+var currentScriptPath = scripts[scripts.length-1].src;
+
 angular.module('tstechly.angular-bootstrap-dualselect', []).directive('tsDualselect', [ function () {
 
   return {
     restrict: 'E',
     scope:true,
-    templateUrl: 'dualselect.html',
+    templateUrl: currentScriptPath.replace('bootstrap-dualselect.js', 'dualselect.html'),
     controller: 'DualselectCtrl',
     link: function (scope, elements, attrs) {
       scope.label1 = attrs.label1;
       scope.label2 = attrs.label2;
-      scope.allItems = scope.$parent.list;
-      scope.selectedItems = scope.$parent.list2;
+      scope.allItems = scope.$parent[attrs.ngOptions];
+      scope.selectedItems = scope.$parent[attrs.ngModel];
     }
   };
 
