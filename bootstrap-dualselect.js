9ccb9caa43fd990acc9e2bb5ecb7f96ae8bdd170
Tomasz <tomasz@stechly.com>
2014-07-05 08:51:42 -0700

    Initiate all-items array with items that are not on selected items list

--- bootstrap-dualselect.js
+++ bootstrap-dualselect.js
@@ -3,8 +3,19 @@
 var scripts = document.getElementsByTagName("script")
 var currentScriptPath = scripts[scripts.length-1].src;
 
-angular.module('tstechly.angular-bootstrap-dualselect', []).directive('tsDualselect', [ function () {
+/* returns true if array 'a' contains 'obj' */
+function contains(a, obj) {
+  var i = a.length;
+  while (i--) {
+    if (a[i] === obj) {
+      return true;
+    }
+  }
+  return false;
+}
 
+
+angular.module('tstechly.angular-bootstrap-dualselect', []).directive('tsDualselect', [ function () {
   return {
     restrict: 'E',
     scope:true,
@@ -13,8 +24,15 @@ angular.module('tstechly.angular-bootstrap-dualselect', []).directive('tsDualsel
     link: function (scope, elements, attrs) {
       scope.label1 = attrs.label1;
       scope.label2 = attrs.label2;
-      scope.allItems = scope.$parent[attrs.ngOptions];
       scope.selectedItems = scope.$parent[attrs.ngModel];
+
+      /* Add only items that are not on selectedItems list */
+      for(var idx in scope.$parent[attrs.ngOptions]) {
+        var item = scope.$parent[attrs.ngOptions][idx];
+        if(!contains(scope.selectedItems, item)) {
+          scope.allItems.push(item);
+        }
+      }
 
       scope.allItems.sort(function(a,b) { return a.toLowerCase().localeCompare(b.toLowerCase()); });
       scope.fAllItems.sort(function(a,b) { return a.toLowerCase().localeCompare(b.toLowerCase()); });
