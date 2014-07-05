454cbdafaeae38dacd5e4f2977e81b1fba053c36
Tomasz <tomasz@stechly.com>
2014-07-05 07:13:37 -0700

    Improve example - make meaningful name of variables. Add list of all US States

--- pages/home/HomeCtrl.js
+++ pages/home/HomeCtrl.js
@@ -1,6 +1,58 @@
 'use strict';
 
 angular.module('myApp').controller('HomeCtrl', ['$scope', function ($scope) {
-  $scope.list = ['Arizona', 'Alaska', 'Alabama'];
-  $scope.list2 = ['Florida', 'Kansas'];
+  $scope.selectedStates = ['HAWAII', 'IOWA', 'ALASKA'];
+
+  $scope.allStates = [
+    "ALABAMA",
+    "ALASKA",
+    "ARIZONA",
+    "ARKANSAS",
+    "CALIFORNIA",
+    "COLORADO",
+    "CONNECTICUT",
+    "DELAWARE",
+    "FLORIDA",
+    "GEORGIA",
+    "HAWAII",
+    "IDAHO",
+    "ILLINOIS",
+    "INDIANA",
+    "IOWA",
+    "KANSAS",
+    "KENTUCKY",
+    "LOUISIANA",
+    "MAINE",
+    "MARYLAND",
+    "MASSACHUSETTS",
+    "MICHIGAN",
+    "MINNESOTA",
+    "MISSISSIPPI",
+    "MISSOURI",
+    "MONTANA",
+    "NEBRASKA",
+    "NEVADA",
+    "NEW HAMPSHIRE",
+    "NEW JERSEY",
+    "NEW MEXICO",
+    "NEW YORK",
+    "NORTH CAROLINA",
+    "NORTH DAKOTA",
+    "OHIO",
+    "OKLAHOMA",
+    "OREGON",
+    "PENNSYLVANIA",
+    "RHODE ISLAND",
+    "SOUTH CAROLINA",
+    "SOUTH DAKOTA",
+    "TENNESSEE",
+    "TEXAS",
+    "UTAH",
+    "VERMONT",
+    "VIRGINIA",
+    "WASHINGTON",
+    "WEST VIRGINIA",
+    "WISCONSIN",
+    "WYOMING"
+  ];
 }]);
